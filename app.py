42f8a133f0a1b41133b0590a53b13936300317a3
VaishnaviNakka2006 <vaishunakka2006@gmail.com>
2026-04-01 15:36:53 +0530

    Added new

--- app.py
+++ app.py
@@ -68,6 +68,25 @@ def done(id):
     conn.commit()
 
     return redirect("/")
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit(id):
+    conn = get_db()
+
+    if request.method == "POST":
+        new_name = request.form["name"]
+        conn.execute("UPDATE habits SET name=? WHERE id=?", (new_name, id))
+        conn.commit()
+        return redirect("/")
+
+    habit = conn.execute("SELECT * FROM habits WHERE id=?", (id,)).fetchone()
+    return render_template("edit.html", habit=habit)
+@app.route("/delete/<int:id>")
+def delete(id):
+    conn = get_db()
+    conn.execute("DELETE FROM habits WHERE id=?", (id,))
+    conn.commit()
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
